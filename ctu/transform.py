8044594c2a41528c300fcbb9cccc75cbe374297f
Ali M. Farahani <45420649+a-m-farahani@users.noreply.github.com>
2020-08-12 10:47:16 +0430

    Update transform.py

--- ctu/transform.py
+++ ctu/transform.py
@@ -1,7 +1,9 @@
 from ctu import reader, display
 from scipy import ndimage
+from sklearn import cluster
 import skimage
 import numpy as np
+from matplotlib import pyplot as plt
 
 def BodyMask(img, threshold=-400):
     in_img = img.copy()
@@ -22,3 +24,47 @@ def BodyMask(img, threshold=-400):
     int_mask = logical_mask.astype(int)
     masked_img = (in_img + abs(in_img.min())) * int_mask
     return int_mask, masked_img 
+
+
+def LungMask(img):
+    img_shape = img.shape
+    
+    mean = np.mean(img)
+    std = np.std(img)
+    img = img-mean
+    img = img/std
+    
+    middle = img[int(img_shape[1]/5):int(img_shape[1]/5*4),int(img_shape[0] /5):int(img_shape[0] /5*4)] 
+    mean = np.mean(middle)  
+    max = np.max(img)
+    min = np.min(img)
+    
+    img[img==max]=mean
+    img[img==min]=mean
+    
+    kmeans = cluster.KMeans(n_clusters=2).fit(np.reshape(middle,[np.prod(middle.shape),1]))
+    centers = sorted(kmeans.cluster_centers_.flatten())
+    threshold = np.mean(centers)
+    thresh_img = np.where(img<threshold,1.0,0.0)  
+
+    eroded = skimage.morphology.erosion(thresh_img,np.ones([3,3]))
+    dilation = skimage.morphology.dilation(eroded,np.ones([8,8]))
+
+    labels = skimage.measure.label(dilation) 
+    label_vals = np.unique(labels)
+    regions = skimage.measure.regionprops(labels)
+    good_labels = []
+    for prop in regions:
+        img_shape = img.shape
+        B = prop.bbox
+        if B[2]-B[0]<img_shape[0] /10*9 and B[3]-B[1]<img_shape[1]/10*9 and B[0]>img_shape[0] /5 and B[2]<img_shape[1]/5*4:
+            img_shape = img.shape
+            good_labels.append(prop.label)
+    mask = np.ndarray([img_shape[0] ,img_shape[1]],dtype=np.int8)
+    mask[:] = 0
+
+    for N in good_labels:
+        mask = mask + np.where(labels==N,1,0)
+    mask = skimage.morphology.dilation(mask,np.ones([10,10])) 
+
+    return labels,mask, mask*img
