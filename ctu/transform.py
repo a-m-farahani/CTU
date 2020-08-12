fba1d53f482c8b5ed748d1983bdb62c2cfcc3934
Ali M. Farahani <45420649+a-m-farahani@users.noreply.github.com>
2020-08-12 09:51:52 +0430

    Update transform.py

--- ctu/transform.py
+++ ctu/transform.py
@@ -3,20 +3,19 @@ from scipy import ndimage
 import skimage
 import numpy as np
 
-def BodyMask(img):
-    maskthreshold = -500
+def BodyMask(img, threshold=-400):
     img_shape = img.shape
     img = ndimage.zoom(img, 256/np.asarray(img.shape), order=0)
-    bodymask = img > maskthreshold
+    bodymask = img > threshold
     bodymask = ndimage.binary_closing(bodymask)
     bodymask = ndimage.binary_fill_holes(bodymask, structure=np.ones((3, 3))).astype(int)
-    bodymask = ndimage.binary_erosion(bodymask, iterations=2)
+    bodymask = ndimage.binary_erosion(bodymask, iterations=1)
     bodymask = skimage.measure.label(bodymask.astype(int), connectivity=1)
     regions = skimage.measure.regionprops(bodymask.astype(int))
     if len(regions) > 0:
         max_region = np.argmax(list(map(lambda x: x.area, regions))) + 1
         bodymask = bodymask == max_region
-        bodymask = ndimage.binary_dilation(bodymask, iterations=2)
-    real_scaling = np.asarray(img_shape)/128
+        bodymask = ndimage.binary_dilation(bodymask, iterations=1)
+    real_scaling = np.asarray(img_shape)/256
     logical_mask = ndimage.zoom(bodymask, real_scaling, order=0)
     return logical_mask.astype(int)
